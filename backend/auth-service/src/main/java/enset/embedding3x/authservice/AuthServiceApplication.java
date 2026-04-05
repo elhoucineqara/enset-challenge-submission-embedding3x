cf697621b54191518b8d34572be7e9fabf53dfc7
badr <fadre6@gmail.com>
2026-04-05 18:26:40 +0100

    feat: initialize auth-service backend with Spring Boot project structure and security configuration

---
+++ backend/auth-service/src/main/java/enset/embedding3x/authservice/AuthServiceApplication.java
@@ -0,0 +1,13 @@
+package enset.embedding3x.authservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+
+}
---
+++ backend/auth-service/src/test/java/enset/embedding3x/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package enset.embedding3x.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
